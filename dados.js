761a1a81a2300d86fc129b9df1199e72802394e6
mari519 <m.emiliarodrigues27@gmail.com>
2024-09-08 23:45:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo : "Crie seu Currículo do Zero",
+        descricao : "Um currículo bem estruturado é a sua porta de entrada para o mercado de trabalho. É como um cartão de visitas digital que resume suas qualificações e experiências profissionais. Um documento claro, conciso e visualmente atraente aumenta significativamente suas chances de conseguir uma entrevista.",
+        link: "https://www.linkedin.com/search/results/all/?keywords=curriculum%20vitae&origin=RELATED_SEARCH_FROM_SRP&sid=%2COK"
+        
+    },
+    {
+        titulo : "Especialize-se Gratuitamente",
+        descricao : "Aumente suas chances no mercado de trabalho com especializações gratuitas em diversas áreas de atuação com diversas empresas e instituições renomadas.",
+        link: "https://linktr.ee/projectextensi"
+        
+    },
+    {
+        titulo : "Descubra sua Vocação",
+        descricao : "Ainda não sabe qual profissão seguir? Um teste vocacional pode te ajudar a descobrir suas habilidades e interesses, indicando as áreas de atuação mais adequadas para você.",
+        link: "https://admission.worka.love/chat/faculdade-descomplica/?mp_id=%24device%3A191ce9fb09d1d69-04d183b3989e7b-26001151-144000-191ce9fb09d1d69&utm_source=google&utm_medium=cpc&utm_campaign=gmax_ug_performance_gwt-paid-media_meio_conversao_always-on_lead&utm_content=especifica-brasil&utm_term=especifica-brasil"
+        
+   
+    }
+]
+
+ 
